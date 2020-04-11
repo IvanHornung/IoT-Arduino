641a310e5a27c00bb5905f23c606a53d253e2427
Ivan Hornung <ivanhor2003@gmail.com>
2020-04-10 22:06:11 -0700

    Update Fibonacci.c

--- Learning C/Assignments/Fibonacci.c
+++ Learning C/Assignments/Fibonacci.c
@@ -3,18 +3,21 @@ in the Fibonacci sequence. You will need to look up the definition of the Fibona
 if you don't know it. The first two numbers in the sequence are 0 and 1, but your program should
 compute the next four digits. Turn in your C program. Your C program must compile in order for it to be tested.*/
 
-public int fibonacci(int index){
-        if(n == 0 || n == 1) 
-            return 1;
-        return fibonacci(n-1) + fibonacci(n-2);
+#include <stdio.h>
+
+int fibonacci(int index) {
+    if (index == 0 || index == 1)
+        return 1;
+    return fibonacci(index - 1) + fibonacci(index - 2);
 }
 
 void main() {
     int i;
-    for(i = 0; i < 6; i++) {
-        if(i == 5) {
+    for (i = 0; i < 6; i++) {
+        if (i == 5) {
             printf("%i", fibonacci(i));
-            break; }            
+            break;
+        }
         printf("%i, ", fibonacci(i));
     }
 }
