834f6b55ee021aa47b373a8390ae5836a2085d8f
Ivan Hornung <ivanhor2003@gmail.com>
2020-04-12 19:48:31 -0700

    Update OnesAndZeros.ino

--- Arduino Platform/OnesAndZeros.ino
+++ Arduino Platform/OnesAndZeros.ino
@@ -1,19 +1,21 @@
+/*Write a program that allows the user to control the LED connected to pin 13 of the Arduino. 
+When the program is started, the LED should be off. The user should open the serial monitor to communicate with the Arduino. 
+If the user sends the character '1' through the serial monitor then the LED should turn on. If the user sends the character '0' 
+through the serial monitor then the LED should turn off.
+If you do not have an Arduino, you can use the web-based Arduino simulator at www.tinkercad.com. 
+You will need to create an account for free. There are instructional videos on that website that will teach you how to use the simulator.*/
+
 void setup() {
   Serial.begin(9600, SERIAL_8N1);
   pinMode(13, OUTPUT);
-  digitalWrite(13, LOW); //just to be safe
 }
 
+char cmd;
 void loop() {
- if(Serial.available()!= 0) {
-    if(Serial.read() == 1) {
-      digitalWrite(13, HIGH);
-      Serial.println("LED ON");
-    }
-    else if(Serial.read() == 0) {
-      digitalWrite(13, LOW);
-      Serial.println("LED OFF"); }
-    else
-      Serial.println("No data is available.");
-  }
+  while(Serial.available())
+    cmd = Serial.read();
+  if(cmd == '1')
+    digitalWrite(13,HIGH);
+  else if(cmd == '0')
+    digitalWrite(13,LOW);
 }
